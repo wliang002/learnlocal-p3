9ec783e2d63a37d17b5ef5368fe487794133726e
wliang002 <wliang002@berkeley.edu>
2019-07-16 02:48:14 -0700

    login reg pages

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,11 +1,36 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Login = () => {
-    return (
-        <div>
-            login
+  return (
+    <div className='container'>
+      <div className='alert alert-danger'>
+          Invalid credentials
+      </div>
+      <h1 className='large text-primary'>Sign In</h1>
+      <p className='lead'><i className='fas fa-user' /> Sign into Your Account</p>
+      <form className='form' action='dashboard.html'>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            required
+          />
         </div>
-    )
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+          />
+        </div>
+        <input type='submit' className='btn btn-primary' value='Login' />
+      </form>
+      <p className='my-1'>
+          Don't have an account? <Link to='/register'>Register</Link>
+      </p>
+    </div>
+  )
 }
 
 export default Login
