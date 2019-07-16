3aa5f820ac8095553f97a2030939c0e4a5a55516
birbjam <41635757+birbjam@users.noreply.github.com>
2019-07-15 20:40:50 -0700

    Teach component form added, added to app.js for visual testing

--- src/App.js
+++ src/App.js
@@ -1,30 +1,62 @@
-import React from 'react'
+import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import TeachForm from './components/TeachForm'
 
-function App () {
-  return (
-    <div className='App'>
-      <Header />
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-      <Footer />
-    </div>
-  )
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <Header />
+        {/* Testing out the TeachForm */}
+
+        <TeachForm />
+        {/* Original boilerplate React Code Below: */}
+        {/* <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Learn React
+          </a>
+        </header> */}
+        
+        <Footer />
+      </div>
+    )
+  }
 }
+
+// function App () {
+//   return (
+//     <div className='App'>
+//       <Header />
+//       <header className='App-header'>
+
+//         <img src={logo} className='App-logo' alt='logo' />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className='App-link'
+//           href='https://reactjs.org'
+//           target='_blank'
+//           rel='noopener noreferrer'
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//       <Footer />
+//     </div>
+//   )
+// }
 
 export default App
