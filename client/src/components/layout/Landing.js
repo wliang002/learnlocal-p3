52ff516d2bea753f8e264caf230fc2720771f3e3
Swiftlet <hettybootcamp@gmail.com>
2019-07-17 22:34:48 -0700

    added styles to the sign up log in buttons on the landing.js

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,21 +1,21 @@
 import React from 'react'
+import './Landing.css'
 import { Link, Redirect } from 'react-router-dom'
 
 const Landing = () => {
   return (
     <section className='landing'>
-    
-        <div className='landing-inner'>
-          <div className='buttons'>
-            <Link to='/register' className='btn btn-primary'>
-              Sign Up
-            </Link>
-            <Link to='/login' className='btn btn-light'>
-              Login
-            </Link>
-          </div>
+      <div className='landing-inner'>
+        <p>Sign up or log in to host a class of your own.</p>
+        <div className='buttons'>
+          <Link to='/register' className='btn btn-success'>
+            Sign Up
+          </Link>
+          <Link to='/login' className='btn btn-success'>
+            Login
+          </Link>
         </div>
-
+      </div>
     </section>
 
   )
