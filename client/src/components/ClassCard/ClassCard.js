91a95959593203bb0778b01cb077b68ab908fa1a
birbjam <41635757+birbjam@users.noreply.github.com>
2019-07-20 12:07:24 -0700

    classcard done and styled

--- client/src/components/ClassCard/ClassCard.js
+++ client/src/components/ClassCard/ClassCard.js
@@ -2,11 +2,29 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './ClassCard.css'
 
-const ClassCard = props => {
-  return [<div className='class-card'>
-    <h1>Class:</h1>
+const ClassCard = props => (
+  <div className="class-card">
+    <h1>Class: Wooden heart pendants</h1>
+    <p>
+      <strong>Taught by:</strong> Hetty Chin
+    </p>
+    <p>
+      <strong>Location:</strong> 124 Bolinas Road, Fairfax, CA 94930
+    </p>
+    <p>
+      <strong>Date:</strong> Wednesday, June 12th, 2019
+    </p>
+    <p>
+      <strong>Time:</strong> 2:30PM - 5:30PM
+    </p>
+    <p>
+      <strong>Description:</strong> Wooden blanks and carving tools provided.
+      Beginners welcome. Come learn how to carve a wooden heart pendant, add an
+      eyelet, and braid a new cord for your new necklace.{" "}
+    </p>
+
+    <button className="class-signup-btn">Sign Up</button>
   </div>
-  ]
-}
+);
 
 export default ClassCard
