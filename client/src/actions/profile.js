054023a56f9aeb5b7fdbc12338e728c8b63c08fa
wliang002 <wliang002@berkeley.edu>
2019-07-22 04:39:38 -0700

    add class

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -3,7 +3,9 @@ import { setAlert } from './alert'
 
 import {
   GET_PROFILE,
-  PROFILE_ERROR
+  PROFILE_ERROR,
+  UPDATE_PROFILE,
+  CLEAR_PROFILE
 } from './types'
 
 // Get current users profile
@@ -52,6 +54,39 @@ export const createProfile = (
   } catch (err) {
     const errors = err.response.data.errors
     // validation error
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')))
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    })
+  }
+}
+
+// Add Classes
+export const addClasses = (formData, history) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    // make a put request
+    const res = await axios.put('/api/profile/events', formData, config)
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    })
+
+    dispatch(setAlert('Class Added', 'success'))
+
+    history.push('/dashboard')
+  } catch (err) {
+    const errors = err.response.data.errors
+
     if (errors) {
       errors.forEach(error => dispatch(setAlert(error.msg, 'danger')))
     }
