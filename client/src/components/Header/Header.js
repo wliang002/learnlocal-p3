7a081d73e0c897c1fecc177dec104838cf494ac2
wliang002 <wliang002@berkeley.edu>
2019-07-22 00:45:03 -0700

    logout

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,89 +1,127 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
 import './header.css'
 import CollapsibleHeader from './CollapsibleHeader'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { logout } from '../../actions/auth'
 
-const Header = props => (
-  <header className='site-header'>
-    {/* for wide desktop */}
-    <div className='wideView'>
-      <div className='site-logo'>
-        <Link to='/' className='home'>Learn Local</Link>
-      </div>
-      {/* class categories */}
-      <div className='dropdown first'>
-        <a
-          className='btn btn-secondary dropdown-toggle'
-          href='#'
-          role='button'
-          id='categoryDropdownMenuLink'
-          data-toggle='dropdown'
-          aria-haspopup='true'
-          aria-expanded='false'
-        >
-          Browse Classes
+const Header = ({ auth: { isAuthenticated, loading }, logout }) => {
+  const authLinks = (
+    <div style={{ float: 'right' }}>
+      <div className='LogInBtns'>
+        <Link to='/profile' className='btn btn-success'>
+          Profile
+        </Link>
+        <a onClick={logout} href='#!'>
+          <i className='fas fa-sign-out-alt' />{' '}
+          <span className='hide-sm'>Logout</span>
         </a>
-        <div className='dropdown-content'>
-          <Link to='/learn-by-category' className='dropdown-item'>Category placeholder</Link>
-          <a className='dropdown-item' href='#'>
-            Creative
-          </a>
-          <a className='dropdown-item' href='#'>
-            Business
-          </a>
-          <a className='dropdown-item' href='#'>
-            Lifestyle
-          </a>
-          <a className='dropdown-item' href='#'>
-            Other
-          </a>
-        </div>
-      </div>
-      {/* teachers */}
-      <div className='dropdown last'>
-        <a
-          className='btn btn-secondary dropdown-toggle'
-          href='#'
-          role='button'
-          id='teacherDropdownMenuLink'
-          data-toggle='dropdown'
-          aria-haspopup='true'
-          aria-expanded='false'
-        >
-          Browse Teachers
-        </a>
-        <div className='dropdown-content'>
-          <Link to='/teacher-profile' className='dropdown-item'>TeacherProfile placeholder</Link>
-          <a className='dropdown-item' href='#'>
-            dummy teacher 2
-          </a>
-          <a className='dropdown-item' href='#'>
-            dummy teacher 3
-          </a>
-          <a className='dropdown-item' href='#'>
-            dummy teacher 4
-          </a>
-        </div>
-      </div>
-      <div style={{ float: 'right' }}>
-        <div className='LogInBtns'>
-          <Link to='/login' className='btn btn-success'>
-            Login
-          </Link>
-          <Link to='/register' className='btn btn-success'>
-            Create Account
-          </Link>
-        </div>
       </div>
     </div>
-    {/* for narrower and mobile view */}
-    <div className='narrowView'>
-      <div className='site-logo'>
-        <Link to='/' className='home'>Learn Local</Link>
-      </div>
-      <CollapsibleHeader />
-    </div>
-  </header>
-)
+  )
 
-export default Header
+  const guestLinks = (
+    <div style={{ float: 'right' }}>
+      <div className='LogInBtns'>
+        <Link to='/login' className='btn btn-success'>
+          Login
+        </Link>
+        <Link to='/register' className='btn btn-success'>
+          Create Account
+        </Link>
+      </div>
+    </div>
+  )
+
+  return (
+    <header className='site-header' >
+      {/* for wide desktop */}
+      <div className='wideView' >
+        <div className='site-logo'>
+          <Link to='/' className='home'>Learn Local</Link>
+        </div>
+        {/* class categories */}
+        <div className='dropdown first'>
+          <a
+            className='btn btn-secondary dropdown-toggle'
+            href='#'
+            role='button'
+            id='categoryDropdownMenuLink'
+            data-toggle='dropdown'
+            aria-haspopup='true'
+            aria-expanded='false'
+          >
+            Browse Classes
+          </a>
+          <div className='dropdown-content'>
+            <Link to='/learn-by-category' className='dropdown-item'>Category placeholder</Link>
+            <a className='dropdown-item' href='#'>
+              Creative
+            </a>
+            <a className='dropdown-item' href='#'>
+              Business
+            </a>
+            <a className='dropdown-item' href='#'>
+              Lifestyle
+            </a>
+            <a className='dropdown-item' href='#'>
+              Other
+            </a>
+          </div>
+        </div>
+        {/* teachers */}
+        <div className='dropdown last'>
+          <a
+            className='btn btn-secondary dropdown-toggle'
+            href='#'
+            role='button'
+            id='teacherDropdownMenuLink'
+            data-toggle='dropdown'
+            aria-haspopup='true'
+            aria-expanded='false'
+          >
+            Browse Teachers
+          </a>
+          <div className='dropdown-content'>
+            <Link to='/teacher-profile' className='dropdown-item'>TeacherProfile placeholder</Link>
+            <a className='dropdown-item' href='#'>
+              dummy teacher 2
+            </a>
+            <a className='dropdown-item' href='#'>
+              dummy teacher 3
+            </a>
+            <a className='dropdown-item' href='#'>
+              dummy teacher 4
+            </a>
+          </div>
+        </div>
+
+      </div >
+      {/* for narrower and mobile view */}
+      <div className='narrowView' >
+        <div className='site-logo'>
+          <Link to='/' className='home'>Learn Local</Link>
+        </div>
+        <CollapsibleHeader />
+      </div >
+      {!loading && (
+        <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+      )}
+    </header >
+  )
+}
+
+Header.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+  auth: state.auth
+})
+
+export default connect(
+  mapStateToProps,
+  { logout }
+)(Header)
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -4,7 +4,8 @@ import {
   USER_LOADED,
   AUTH_ERROR,
   LOGIN_SUCCESS,
-  LOGIN_FAIL
+  LOGIN_FAIL,
+  LOGOUT
 
 } from '../actions/types'
 
@@ -39,6 +40,7 @@ export default (state = initialState, action) => {
     case REGISTER_FAIL:
     case AUTH_ERROR:
     case LOGIN_FAIL:
+    case LOGOUT:
       // clear token from local storage
       localStorage.removeItem('token')
       return {
