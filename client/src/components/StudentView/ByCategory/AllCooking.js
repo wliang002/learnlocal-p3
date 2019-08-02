9bc9110d6faa5b40c58a9eeba32a9fe1fc7e9844
Swiftlet <hettybootcamp@gmail.com>
2019-08-01 19:39:59 -0700

    made delete red, update green, colored the icons for each class

--- client/src/components/StudentView/ByCategory/AllCooking.js
+++ client/src/components/StudentView/ByCategory/AllCooking.js
@@ -1,56 +1,56 @@
-import React, { Fragment, useEffect } from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { getProfiles } from "../../../actions/profile";
-import "../../ClassDisplays/ClassDisplays.css";
-import ClassCard from "../../ClassCard/ClassCard";
+import React, { Fragment, useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { getProfiles } from '../../../actions/profile'
+import '../../ClassDisplays/ClassDisplays.css'
+import ClassCard from '../../ClassCard/ClassCard'
 
 const AllCooking = ({ getProfiles, profile: { profiles, loading } }) => {
   useEffect(() => {
-    getProfiles();
-  }, [getProfiles]);
+    getProfiles()
+  }, [getProfiles])
   return (
     <Fragment>
-      <div className="LearnByCategoryContainer">
+      <div className='LearnByCategoryContainer'>
         <h1>Learn!</h1>
         <hr />
         <h2>
-          <i className="fas fa-utensils" />
-          &nbsp;Sign up for a <span class="selectedCategory">Cooking</span> class
+          <i className='fas fa-utensils' />
+          &nbsp;Sign up for a <span class='selectedCategory'>Cooking</span> class
           happening in your&nbsp;neighborhood.
         </h2>
-        <div className="AppDescriptionContainer">
+        <div className='AppDescriptionContainer'>
           <p>
             If there’s a class you’d like to take, click the “Sign up” buttom
             to&nbsp;RSVP.
           </p>
         </div>
         <hr />
-        <div className="profiles">
+        <div className='profiles'>
           {profiles.length > 0 ? (
             profiles.map(profile => (
               <ClassCard
-                event={profile.events.filter(e => e.eventType === "Cooking")}
+                event={profile.events.filter(e => e.eventType === 'Cooking')}
               />
             ))
           ) : (
-            <p className="sorry">
+            <p className='sorry'>
               We&rsquo;re sorry, we did not find any classes in that category...
             </p>
           )}
         </div>
       </div>
     </Fragment>
-  );
-};
+  )
+}
 AllCooking.propTypes = {
   getProfiles: PropTypes.func.isRequired,
   profile: PropTypes.object.isRequired
-};
+}
 const mapStateToProps = state => ({
   profile: state.profile
-});
+})
 export default connect(
   mapStateToProps,
   { getProfiles }
-)(AllCooking);
+)(AllCooking)
