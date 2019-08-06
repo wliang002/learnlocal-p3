5ed44e8ede35cd16997a5ad2bf7c749e970eb952
Swiftlet <hettybootcamp@gmail.com>
2019-08-05 19:11:41 -0700

    fixed residual mobile responsive issues

--- client/src/components/ClassCard/MapBox.js
+++ client/src/components/ClassCard/MapBox.js
@@ -4,7 +4,6 @@ import axios from 'axios'
 import GeoCard from './GeoCard'
 import StickyBox from 'react-sticky-box'
 
-
 const Map = ReactMapboxGl({
   accessToken: 'pk.eyJ1IjoibGVhcm5sb2NhbCIsImEiOiJjanlkZ2dram4wcHY1M2ptZm8wNGZrNzVkIn0.SnFaLsmeVQkX5XSj8-pp-A'
 })
